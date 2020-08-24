83f1b4bf652e68a5367328c147d3685d2b9e1ec5
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-24 16:50:57 +0000

    initial

---
+++ resources/views/admin/layout/master.blade.php
@@ -0,0 +1,8 @@
+@include('admin.layout.navbar')
+@include('admin.layout.sidebar')
+ <div id="layoutSidenav_content">
+    <main>
+   @yield('content')
+</main>
+@include('admin.layout.footer')   
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@include('admin.layout.navbar')
+@include('admin.layout.sidebar')
+@include('admin.layout.content')
+@include('admin.layout.footer')
